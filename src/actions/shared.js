5a4886a8622f72ce4011e6fc8894fe048ba9a17e
Gon Gis <gon.eu.1987@gmail.com>
2020-02-28 09:31:01 +0100

    adding loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from "../utils/api";
 import { receieveUsers } from "./users";
 import { receiveTweets } from "./tweets";
 import { setAuthedUser } from "./authedUser";
+import { showLoading, hideLoading } from "react-redux-loading";
 
 const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData () {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData()
             .then(({users, tweets }) => {
                 dispatch(receieveUsers(users))
                 dispatch(receiveTweets(tweets))
                 dispatch(setAuthedUser(AUTHED_ID))
-        })
+                dispatch(hideLoading())
+            })
     }
 }
