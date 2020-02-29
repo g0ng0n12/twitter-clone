f4fdb6086f0f9877e288a65eca0916d6ab3162b0
Gon Gis <gon.eu.1987@gmail.com>
2020-02-29 12:57:39 +0100

    adding react router with the needed routes

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,7 @@ import { TiArrowBackOutline } from 'react-icons/ti'
 import { TiHeartOutline } from 'react-icons/ti'
 import { TiHeartFullOutline } from 'react-icons/ti'
 import { handleToggleTweet} from "../actions/tweets";
+import { Link, withRouter } from 'react-router-dom';
 
 class Tweet extends Component {
 
@@ -23,6 +24,8 @@ class Tweet extends Component {
         e.preventDefault();
         // todo redirect to parent tweet
 
+        this.props.history.push(`/tweet/${id}`)
+
     };
 
 
@@ -39,7 +42,7 @@ class Tweet extends Component {
         } = tweet;
 
         return (
-            <div className='tweet'>
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img src={avatar} alt={`Avatar of ${name}`} className='avatar' />
                 <div className='tweet-info'>
                     <div>
@@ -64,7 +67,7 @@ class Tweet extends Component {
                         <span>{likes !== 0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -82,4 +85,4 @@ function mapStateToProps({ authedUser, users, tweets}, { id }){
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(onnect(mapStateToProps)(Tweet))
